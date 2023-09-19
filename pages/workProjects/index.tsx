8f07e27adf610bf4a442db25d7d8a4046947d950
Loxi <dianyuyi@gmail.com>
2023-09-19 20:41:04 +0800

    fix: Fetch list

--- pages/workProjects/index.tsx
+++ pages/workProjects/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { END } from 'redux-saga'
+// import { END } from 'redux-saga'
 import { useTranslation } from 'react-i18next'
 
 import { wrapper } from 'src/redux/store'
-import { getDatabaseRequest } from 'src/redux_saga/server/getDatabase/actions'
+// import { getDatabaseRequest } from 'src/redux_saga/server/getDatabase/actions'
+import { getDatabaseAPI } from 'server/notion/getDataBaseAPI'
 import { usePageCollect } from 'src/hook'
 import Layout from 'src/components/layout'
 import WorkListPage from 'src/components/containers/workProjects/list'
 
-const WorkProjects = (): JSX.Element => {
+const WorkProjects = ({ database }: { database: Notion.Database }): JSX.Element => {
   const { t } = useTranslation()
 
-  const database = useSelector((state: Store.RootState) => state.server.databaseSlice.response)
+  // const database = useSelector((state: Store.RootState) => state.server.databaseSlice.response)
 
   const languageCode = useSelector(
     (state: Store.RootState) => state.client.languageCodeSlice.languageCode
@@ -27,13 +28,17 @@ const WorkProjects = (): JSX.Element => {
 }
 
 export const getStaticProps = wrapper.getStaticProps((store) => async () => {
-  store.dispatch(getDatabaseRequest({ type: 'work-projects' }))
-  store.dispatch(END)
-  await store.sagaTask.toPromise()
+  // store.dispatch(getDatabaseRequest({ type: 'work-projects' }))
+  // store.dispatch(END)
+  // await store.sagaTask.toPromise()
+
+  const response = await getDatabaseAPI('work-projects')
 
   return {
-    props: {},
-    revalidate: 60 * 5,
+    props: {
+      database: response?.results ?? [],
+    },
+    revalidate: 60 * 30,
   }
 })
 
